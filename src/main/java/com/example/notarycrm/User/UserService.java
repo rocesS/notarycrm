6439846e145494460cd6a09b9afd57342dbd30fc
Kamil <kamil@klemczak.pl>
2024-03-21 02:28:16 +0100

    feat: User class is able to edit user in data base. Save button and Cancel button work properly.

--- src/main/java/com/example/notarycrm/User/UserService.java
+++ src/main/java/com/example/notarycrm/User/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,4 +15,17 @@ public class UserService {
     public List<User> userList () {
         return (List<User>) repo.findAll();
     }
+
+    public void save(User user) {
+        repo.save(user);
+    }
+
+    public User get(Integer id) throws UserNotFoundException {
+        Optional<User> result = repo.findById(id);
+        if (result.isPresent()) {
+            return result.get();
+        }
+        throw new UserNotFoundException("Could not find any user with ID " + id);
+    }
+
 }
