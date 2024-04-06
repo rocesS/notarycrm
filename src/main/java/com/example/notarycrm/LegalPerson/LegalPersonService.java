ced5f3f4dd3668b9732ddaa80758f0f3b9ea02c9
Kamil <kamil@klemczak.pl>
2024-04-06 03:20:04 +0200

    feat: LegalPersonController edit function done

--- src/main/java/com/example/notarycrm/LegalPerson/LegalPersonService.java
+++ src/main/java/com/example/notarycrm/LegalPerson/LegalPersonService.java
@@ -2,8 +2,14 @@ package com.example.notarycrm.LegalPerson;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LegalPersonService {
@@ -19,7 +25,14 @@ public class LegalPersonService {
         repo.save(legalPerson);
     }
 
-    public LegalPerson get(Integer id) throws LegalPersonNot
+    public LegalPerson get(Integer id) throws LegalPersonNotFoundException {
+        Optional<LegalPerson> result = repo.findById(id);
+        if (result.isPresent()){
+            return result.get();
+        }
+        throw new LegalPersonNotFoundException("Could not find any legal person with ID");
+    }
+
 
 
 }
