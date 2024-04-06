ab4a75c61784cc2944e3472ff699bca59b13c4cc
Kamil <kamil@klemczak.pl>
2024-04-06 03:27:03 +0200

    feat: LegalPersonController edit function done

--- src/main/java/com/example/notarycrm/LegalPerson/LegalPersonService.java
+++ src/main/java/com/example/notarycrm/LegalPerson/LegalPersonService.java
@@ -2,11 +2,6 @@ package com.example.notarycrm.LegalPerson;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
