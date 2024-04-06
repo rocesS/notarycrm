f152132508b15b53cd0d77c3dbcc0006d2606e0f
Kamil <kamil@klemczak.pl>
2024-04-06 03:20:04 +0200

    feat: LegalPersonController edit function done

--- src/main/java/com/example/notarycrm/LegalPerson/LegalPersonController.java
+++ src/main/java/com/example/notarycrm/LegalPerson/LegalPersonController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -34,6 +35,19 @@ public class LegalPersonController {
         service.save(legalPerson);
         ra.addFlashAttribute("message","Legal Person has been saved successfully");
         return "redirect:/legalpersons";
+    }
+
+    @GetMapping("/legalpersons/edit/{id}")
+    public String showEditForm(@PathVariable("id") Integer id, Model model, RedirectAttributes ra) {
+        try {
+            LegalPerson legalPerson = service.get(id);
+            model.addAttribute("legalperson",legalPerson);
+            model.addAttribute("pageTitle", "Edit Legal Person (ID: " + id + ")");
+            return "legalperson_form";
+        } catch (LegalPersonNotFoundException e) {
+            ra.addFlashAttribute("message", e.getMessage());
+            return "redirect:/legalpersons";
+        }
     }
 
 }
