61b2cbb4ad57d6ce7ad4c510af5e09fd493bc4c8
rocess <kamil@klemczak.pl>
2024-05-02 13:17:17 +0200

    Feat: Created KrsValidationException for KrsValidationService class for HTTP requests

---
+++ src/main/java/com/example/notarycrm/LegalPerson/KrsValidationException.java
@@ -0,0 +1,7 @@
+package com.example.notarycrm.LegalPerson;
+
+public class KrsValidationException extends Throwable {
+    public KrsValidationException(String message) {
+        super(message);
+    }
+}
