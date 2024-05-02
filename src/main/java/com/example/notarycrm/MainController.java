b2d2a93eee0a8609cc59f088fcbb85dc0b3ec452
rocess <kamil@klemczak.pl>
2024-05-02 13:15:19 +0200

    Feat: MainController got new Bean "RestTeamplate" for test class: KrsValidatorServiceTest

--- src/main/java/com/example/notarycrm/MainController.java
+++ src/main/java/com/example/notarycrm/MainController.java
@@ -1,11 +1,18 @@
 package com.example.notarycrm;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MainController {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     @GetMapping("")
     public String showHomePage() {
