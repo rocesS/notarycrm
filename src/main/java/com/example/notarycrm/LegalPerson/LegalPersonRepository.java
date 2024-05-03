a42b9c09eb56a721ba3e02ebd07cda1189594035
rocess <kamil@klemczak.pl>
2024-05-03 03:51:01 +0200

    Feat: Added method findByKrsNumberAndIdNot. KRS number is uniq and beacuse of this method we can check our internal database and handle situation when user could save duplicate which is unacceptable.

--- src/main/java/com/example/notarycrm/LegalPerson/LegalPersonRepository.java
+++ src/main/java/com/example/notarycrm/LegalPerson/LegalPersonRepository.java
@@ -2,6 +2,10 @@ package com.example.notarycrm.LegalPerson;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface LegalPersonRepository extends CrudRepository<LegalPerson, Integer>  {
     Long countById(Integer id);
+    //wyszukanie po KRS podanym przez użytkownika
+    Optional<LegalPerson> findByKrsNumberAndIdNot(String krsNumber, Integer id);
 }
