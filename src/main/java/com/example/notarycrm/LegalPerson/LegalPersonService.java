5eefa39207cfd087a6aa3639d13d8db273575417
rocess <kamil@klemczak.pl>
2024-05-03 03:56:25 +0200

    Feat: Added @Validated to provide handling exceptions in whole class. KrsValidatorService has been injected by constructor. Save Legal Person method has been extended and tested

--- src/main/java/com/example/notarycrm/LegalPerson/LegalPersonService.java
+++ src/main/java/com/example/notarycrm/LegalPerson/LegalPersonService.java
@@ -2,21 +2,38 @@ package com.example.notarycrm.LegalPerson;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Validated //zeby obsluzyc  @Pattern(regexp = "\\d{10}", message = "KRS number must consist of 10 digits")
 public class LegalPersonService {
 
+    private final LegalPersonRepository repo;
+    private final KrsValidatorService krsValidatorService;
+
     @Autowired
-    private LegalPersonRepository repo;
+    public LegalPersonService(LegalPersonRepository repo, KrsValidatorService krsValidatorService) {
+        this.repo = repo;
+        this.krsValidatorService = krsValidatorService;
+    }
 
     public List<LegalPerson> legalPersonList() {
         return  (List<LegalPerson>) repo.findAll();
     }
 
-    public void save(LegalPerson legalPerson) {
+    public void save(LegalPerson legalPerson) throws KrsValidationException, DuplicateKrsException {
+        if (!krsValidatorService.validateKrsNumber(legalPerson.getKrsNumber())) {
+            throw new KrsValidationException("Invalid KRS number: " + legalPerson.getKrsNumber());
+        }
+
+        Optional<LegalPerson> existingPersonWithKrs = repo.findByKrsNumberAndIdNot(legalPerson.getKrsNumber(), legalPerson.getId());
+        if (existingPersonWithKrs.isPresent() && !existingPersonWithKrs.get().getId().equals(legalPerson.getId())) {
+            throw new DuplicateKrsException("KRS number: " + legalPerson.getKrsNumber() +  " already exists and is assigned to other legal person");
+        }
+
         repo.save(legalPerson);
     }
 
